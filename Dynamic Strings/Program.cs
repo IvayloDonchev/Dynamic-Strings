1c66520d1ea806b5324beb0f8d0a7f8d96f8e50e
Ivaylo Donchev <idonchev@hotmail.com>
2017-03-19 13:58:46 +0200

    init

--- Dynamic Strings/Program.cs
+++ Dynamic Strings/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// http://www.c-sharpcorner.com/article/how-to-create-dynamic-strings-in-c-sharp/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace Dynamic_Strings
     {
         static void Main(string[] args)
         {
+            int amount = 26;
+            string s1 ="My age is ";
+            s1 = string.Concat(s1, amount.ToString());
         }
     }
 }
