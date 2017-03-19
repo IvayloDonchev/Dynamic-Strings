9f438f6eeb4e5160dad5bb9b5ee6935cb1043efd
Ivaylo Donchev <idonchev@hotmail.com>
2017-03-19 14:17:48 +0200

    ss

--- Dynamic Strings/Program.cs
+++ Dynamic Strings/Program.cs
@@ -11,9 +11,29 @@ namespace Dynamic_Strings
     {
         static void Main(string[] args)
         {
-            int amount = 26;
+            int amount = 45;
             string s1 ="My age is ";
             s1 = string.Concat(s1, amount.ToString());
+            Console.WriteLine(s1);
+
+            StringBuilder sb = new StringBuilder("My age is ");
+            sb.Append(amount);
+            Console.WriteLine(sb);
+
+            sb = new StringBuilder("ABCD");
+            sb.Append("EF");
+            Console.WriteLine(sb);
+            sb.AppendFormat(" {0:n}", 1100);
+            Console.WriteLine(sb);
+            sb.Insert(2, 'Z');
+            Console.WriteLine(sb);
+            sb.Remove(8, 6);
+            Console.WriteLine(sb);
+            sb.Replace("0", "x");
+            Console.WriteLine(sb);
+
+
+            Console.ReadKey();
         }
     }
 }
